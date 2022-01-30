8aec9de6bae1f316a44f9c3849ee7714e66b7110
Hank Liu <hank33liu@gmail.com>
2022-01-29 17:32:26 -0800

    Completed first 7 JavaScript exercises per TOP Fundamentals Part 4.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  const tempC = Math.round((tempF-32)/1.8*10)/10;
+  return tempC;
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  const tempF = Math.round(((tempC*1.8) + 32)*10)/10;
+  return tempF;
 };
 
 // Do not edit below this line
