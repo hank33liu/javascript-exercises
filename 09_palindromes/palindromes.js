b462f8166ca419e3ea2d4821924b557225dfe0d6
Hank Liu <hank33liu@gmail.com>
2022-02-15 00:50:36 -0800

    Completed calculator and palindrome JavaScript exercises.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    const inputSimplifiedAsArray = Array.from(input.replace(/[^a-zA-Z]/g, '').toLowerCase());
+    const inputSimplifiedAsArrayReversed = inputSimplifiedAsArray.slice().reverse();
+    return inputSimplifiedAsArray.join() === inputSimplifiedAsArrayReversed.join();
 };
 
 // Do not edit below this line
