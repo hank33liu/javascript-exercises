1b042e7c1637c92d5595f3fb27ae23fe07884d63
Hank Liu <hank33liu@gmail.com>
2022-02-15 22:56:02 -0800

    Completed JS Exercises fibonacci, getTheTitles, and findtheOldest.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 1) {return 'OOPS'};
+    if (number===1 || number===2) {return 1;}
+    twoBefore = 1;
+    oneBefore = 1;
+    total = 1;
+    for (i=3; i<=number; i++) {
+        twoBefore = oneBefore;
+        oneBefore = total;
+        total = twoBefore + oneBefore;
+    }
+    return total;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce(function(oldestCandidate, person) {
+        !person['yearOfDeath'] ? person['yearOfDeath']=(new Date()).getFullYear() :null;
+        if ((person.yearOfDeath-person.yearOfBirth)>(oldestCandidate.yearOfDeath-oldestCandidate.yearOfBirth)) {
+            oldestCandidate = person;
+        }
+        return oldestCandidate;
+    },{
+        name: "Test",
+        yearOfBirth: 0,
+        yearOfDeath: 0,
+      });
 };
 
 // Do not edit below this line
